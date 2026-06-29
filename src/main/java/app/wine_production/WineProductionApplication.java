337097d6be14368c845c7bc203796203594a00ab
LukaszSz90 <lukasz90sz@gmail.com>
2026-06-29 21:51:52 +0200

    first commit

---
+++ src/main/java/app/wine_production/WineProductionApplication.java
@@ -0,0 +1,13 @@
+package app.wine_production;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineProductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineProductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/wine_production/WineProductionApplicationTests.java
@@ -0,0 +1,13 @@
+package app.wine_production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineProductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
